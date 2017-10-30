3d7976e1cfd39a834ea81bc701f1133a5971d787
theoroe3 <theoroe33@gmail.com>
2017-10-30 10:46:59 +0000

    fig.width = fig.height = 4

--- R/options.R
+++ R/options.R
@@ -37,7 +37,9 @@ set_knitr_options = function(tidy = FALSE,
                              comment = "#>",
                              collapse = TRUE,
                              cache = TRUE,
-                             fig.align = "center"
+                             fig.align = "center",
+                             fig.width = 4,
+                             fig.height = 4
 ) {
   knitr::opts_chunk$set(
     tidy =tidy,
@@ -46,6 +48,8 @@ set_knitr_options = function(tidy = FALSE,
     comment = comment,
     collapse = collapse,
     cache = cache,
-    fig.align=fig.align)
+    fig.align=fig.align,
+    fig.width = fig.width,
+    fig.height = fig.height)
 }
 
