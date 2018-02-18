841da2428bcc97aec976600787a57385b036502b
Colin Gillespie <csgillespie@gmail.com>
2018-02-18 09:10:29 +0000

    Change to pre-push

---
+++ R/create_hook.R
@@ -0,0 +1,9 @@
+#' @export
+#' @rdname  get_logo_path
+create_hook = function() {
+  fname = system.file("extdata", "pre-push", package = "jrNotes", mustWork = TRUE)
+  file.copy(fname, to = ".git/hooks/pre-push", overwrite = TRUE)
+  Sys.chmod(".git/hooks/pre-push", mode = "0700", use_umask = FALSE)
+  message("Added pre-push, now removing pre-commit")
+  file.remove(".git/hooks/pre-commit")
+}
