866621843d6542c82f5afbff76f46b01bb71d901
Colin Gillespie <csgillespie@gmail.com>
2019-08-11 19:06:06 +0100

    Update spelling

--- R/spelling.R
+++ R/spelling.R
@@ -1,27 +1,9 @@
-#' @importFrom spelling spell_check_files
-update_wordlist = function(words, file_name = NULL) {
-  if (is.null(file_name)) {
-    f = file("WORDLIST", "w")
-  } else {
-    old_words = readLines(file_name)
-    f = file(file_name, "w")
-  }
-  on.exit(close(f))
-  words = glue::glue_collapse(c(old_words, words), sep = "\n")
-  cat(words, file = f, append = TRUE)
-}
 get_words = function() {
   fname = system.file("WORDLIST", package = "jrNotes")
   words = readLines(fname, warn = FALSE)
-
-  if (file.exists("WORDLIST")) {
-    words = c(words, readLines("WORDLIST", warn = FALSE))
-  }
   words
 }
 
-#' @rdname check_spelling
-#' @export
 make_wordlist = function(spelling_results) {
   f = file("WORDLIST", "w")
   on.exit(close(f))
@@ -29,22 +11,14 @@ make_wordlist = function(spelling_results) {
   cat(words, file = f, append = TRUE)
 }
 
-#' Spell checkers
+#' @title Spell checkers
 #'
-#' \code{check_spelling} runs a spell check on all c*.Rmd files.
-#'
-#' \code{make_wordlist} takes the output of \code{check_spelling} and
-#' creates a local \code{WORDLIST} file with exceptions. This should be
-#' added to jrNotes.
-#' @param spelling_results A data frame - output of check spelling
-#'
+#' @description check_spelling runs a spell check on all c*.Rmd files.
+#' @importFrom spelling spell_check_files
 #' @export
 check_spelling = function() {
   fnames = list.files(pattern = "^c.*\\.Rmd$")
   words =  get_words()
-  if (file.exists("WORDLIST")) {
-    words = c(words, readLines("WORDLIST", warn = FALSE))
-  }
   message(yellow(symbol$circle_filled, "Spell check (experimental)"))
 
 
@@ -54,13 +28,15 @@ check_spelling = function() {
     return(invisible(NULL))
   }
   msg = glue("{symbol$circle_filled} Any false warnings, please add the word to \\
-              inst/WORDLIST in jrNotes and bump the third digit of the version.")
+              inst/WORDLIST in jrNotes and bump the third digit of the version. \\
+              A file (WORDLIST) has been created in this directory. Just edit \\
+              and append to inst/WORDLIST.")
   message(yellow(msg))
 
   for (i in 1:nrow(in_words)) {
     msg = glue("\t {symbol$fancy_question_mark} {in_words[i, 1]}  {in_words[i, 2]}")
     message(red(msg))
   }
-
-  return(invisible(NULL))
+  make_wordlist(in_words)
+  return(invisible(in_words))
 }
