fe34fcbca7fac23adb431566ad5c9a182f585beb
Rhian Davies <rhian@jumpingrivers.com>
2020-04-14 15:30:09 +0100

    Create addins for live script formatting

---
+++ R/addins.R
@@ -0,0 +1,38 @@
+#' Insert '#> '.
+#'
+#' Call this function as an addin to insert \code{#> } at the cursor position.
+#' @importFrom rstudioapi insertText
+#' @export
+insert_live_comment <- function() {
+  rstudioapi::insertText("#> ")
+}
+
+#' Insert banner
+#'
+#' Call this function as an addin to insert a banner at the cursor position.
+#' @importFrom rstudioapi insertText
+#' @export
+insert_banner <- function() {
+  rstudioapi::insertText("## ===============================================")
+}
+
+
+#' Insert '#> ' in front of highlighted text.
+#'
+#' Call this function as insert \code{#> } at the start of highlighted text.
+#' @importFrom rstudioapi getActiveDocumentContext insertText
+#' @export
+live_comment_highlight <- function() {
+
+  # Gets The active Documeent
+  ctx <- rstudioapi::getActiveDocumentContext()
+
+  # Checks that a document is active
+  if (!is.null(ctx)) {
+    selection = ctx$selection
+    selection_start = selection[[1]]$range$start[1]
+    selection_end = selection[[1]]$range$end[1]
+    pos = Map(c, selection_start:selection_end, 1)
+    rstudioapi::insertText(pos, "#> ")
+  }
+}
