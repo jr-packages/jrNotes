6230a9def19aa095da468a6b3b6c38a8c3a87433
Jamie <jamie@jumpingrivers.com>
2020-04-17 23:50:38 +0100

    add funcitonality to create live python

--- R/create_live.R
+++ R/create_live.R
@@ -75,3 +75,91 @@ create_live_scripts = function() {
 
   msg_ok("Live scripts created and zipped")
 }
+
+
+#' Create live python
+#'
+#' Creates a vm_scripts folder
+#' Creates exercises.ipynb and solutions.ipynb from master_exercises.Rmd
+#' Creates tutor.ipynb and a blank one from master_tutor.Rmd
+#' Zips VM folder ready to be exposed as an artifact
+#'
+#' @importFrom fs dir_ls file_copy
+#' @importFrom zip zipr
+#' @importFrom glue glue
+create_live_python = function() {
+
+  if (!fs::dir_exists("../live")) return(invisible(NULL))
+
+  msg_start("Creating scripts for VM...")
+
+  chapters = list.files(path = "../live/", pattern = "chapter")
+
+  dir.create("../live/vm_scripts", showWarnings = FALSE)
+  unlink(list.files("../live/vm_scripts", full.names = TRUE, include.dirs = TRUE),
+         recursive = TRUE)
+
+  dir.create("../live/tutor_scripts", showWarnings = FALSE)
+  unlink(list.files("../live/tutor_scripts", full.names = TRUE, include.dirs = TRUE),
+         recursive = TRUE)
+
+  for (chapter in chapters) {
+    # Create folder
+    dir.create(glue::glue("../live/vm_scripts/{chapter}", showWarnings = FALSE))
+
+    # Create tutor scripts
+    if (file.exists(glue::glue("../live/{chapter}/master_tutor.Rmd"))) {
+
+      # Create tutor_scripts/chapterX.py
+      system2("sed",
+              args = c(shQuote("/^#>.*/d"), glue::glue("../live/{chapter}/master_tutor.Rmd")),
+              stdout = glue::glue("../live/tutor_scripts/{chapter}.Rmd"))
+      # convert tutor scripts to notebooks
+      system2("jupytext", c("--to", "notebook", glue::glue("../live/tutor_scripts/{chapter}.Rmd")))
+
+      # Create vm_scripts/chapterX/tutor.R
+      system2("sed",
+              args = c(shQuote("s/^#> //"), glue::glue("../live/{chapter}/master_tutor.Rmd")),
+              stdout = glue::glue("../live/vm_scripts/{chapter}/tutor.Rmd"))
+
+      # convert student scripts to notebooks
+      system2("jupytext", c("--to", "notebook", glue::glue("../live/vm_scripts/{chapter}/tutor.Rmd")))
+    }
+
+    # Create exercises and solutions
+    if (file.exists(glue::glue("../live/{chapter}/master_exercises.Rmd"))) {
+
+      # Create exercises_original.R
+      system2("sed",
+              args = c(shQuote("/^#>.*/d"), glue::glue("../live/{chapter}/master_exercises.Rmd")),
+              stdout = glue::glue("../live/vm_scripts/{chapter}/exercises_original.Rmd"))
+      system2("jupytext", c("--to", "notebook", glue::glue("../live/vm_scripts/{chapter}/exercises_original.Rmd")))
+
+      # Create excercises.R
+      fs::file_copy(path = glue::glue("../live/vm_scripts/{chapter}/exercises_original.ipynb"),
+                    new_path = glue::glue("../live/vm_scripts/{chapter}/exercises.ipynb"),
+                    overwrite = TRUE)
+
+      # Create solutions.R
+      system2("sed",
+              args = c(shQuote("s/^#> //"), glue::glue("../live/{chapter}/master_exercises.Rmd")),
+              stdout = glue::glue("../live/vm_scripts/{chapter}/solutions.Rmd"))
+
+      system2("jupytext", c("--to", "notebook", glue::glue("../live/vm_scripts/{chapter}/solutions.Rmd")))
+
+    }
+  }
+
+  # Zip folder
+  msg_start("Zipping scripts...")
+  tutor_scripts = list.files("../live/tutor_scripts", "ipynb$", full.names = TRUE, recursive =  TRUE)
+  vm_scripts = list.files("../live/vm_scripts", "ipynb$", full.names = TRUE, recursive = TRUE)
+  zip::zipr(zipfile = "../live/tutor_scripts.zip", files = tutor_scripts)
+  zip::zipr(zipfile = "../live/vm_scripts.zip", files = vm_scripts)
+
+  # Clean up
+  unlink("../live/vm_scripts/", recursive = TRUE)
+  unlink("../live/tutor_scripts/", recursive = TRUE)
+
+  msg_ok("Live scripts created and zipped")
+}
