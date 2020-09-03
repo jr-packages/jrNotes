9f3eff471d70df49e73e4950a898d877fa49309d
Jack Walton <jwalton3141@gmail.com>
2020-09-03 14:56:09 +0100

    system2 >> system

--- R/check_pkgtitle.R
+++ R/check_pkgtitle.R
@@ -11,8 +11,7 @@ check_pkgtitle = function() {
     pkg_title = packageDescription(r_pkg)$Title
   } else if (language == "python") {
     python_pkg = get_python_pkg_name()
-    pkg_title = system(glue::glue("pip show {python_pkg} | sed -n -e 's/Summary: //p'"),
-                       intern = TRUE)
+    pkg_title = system2("pip", args = c("show", {python_pkg}, "|", "sed", "-n", "-e", "'s/Summary: //p'"), stdout=TRUE)
   } else {
     cli::cli_alert_info("Checking PKG titles not implemented for {language}.")
     return(NULL)
