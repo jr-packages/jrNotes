43863cf43e8746a9294898247eafb2d759108083
JohnMcIntyreJR <john@jumpingrivers.com>
2020-09-09 13:52:59 +0100

    Update check_live_r_file() code

--- R/check_live.R
+++ R/check_live.R
@@ -86,10 +86,14 @@ check_live_r_file = function(fname) {
   ## Check chapter number
   pattern = glue::glue("^## Section {chap_num}\\.\\d*: ")
   check = str_detect(section_hashes, pattern = pattern)
-  line_numbers = which(str_detect(r_code, pattern = pattern))
   if (!all(check)) {
     msg_error("Section should have the form: ## Section X.X: ", padding = 2)
-    msg_error(paste("See lines:", line_numbers, "\n"))
+    error_strings = section_hashes[!check]
+    line_numbers = vector("numeric", length(error_strings))
+    for(pattern in 1:length(error_strings)) {
+      line_numbers[pattern] = which(str_detect(r_code, pattern = error_strings[pattern]))
+    }
+    msg_error(paste("See line:", line_numbers, "\n"))
     stop(call. = FALSE)
   }
 
