32eb63a3d2c57218a175871adb298c97ee63d5d3
Colin Gillespie <csgillespie@gmail.com>
2020-09-21 11:29:07 +0100

    Test that an error is explicitly raised.

--- tests/testthat/test-section_headers.R
+++ tests/testthat/test-section_headers.R
@@ -1,3 +1,5 @@
+# Defining a dummy chapter number
+chapter = "2"
 # Correct headers
 code = c("## ===============================================",
          "## Section 2.1: A simple R session",
@@ -6,15 +8,20 @@ code = c("## ===============================================",
          "## Section 2.2: Assignment operations")
 right_headers = code[stringr::str_detect(code, pattern = "^## \\wec")]
 
-# Incorrect headers
+# Test Correct headers
+test_that("Check section headings (correct)", {
+  expect_true(check_section_headers(code, right_headers, chapter))
+  expect_false(.jrnotes$error)
+})
+
+# Make bad headers
 incorrect_code = c(code[-5], "## Section 2.2 - Assignment operations")
 wrong_headers = incorrect_code[stringr::str_detect(incorrect_code, pattern = "^## \\wec")]
 
-# Defining a dummy chapter number
-chapter = "2"
-
-# Tests
-test_that("check_section_headers errors where appropriate", {
-  expect_equal(check_section_headers(code, right_headers, chapter), NULL)
-  expect_equal(check_section_headers(incorrect_code, wrong_headers, chapter), TRUE)
+# Test bad headers
+test_that("Check section headings (bad)", {
+  expect_equal(check_section_headers(incorrect_code, wrong_headers, chapter), FALSE)
+  expect_message(check_section_headers(incorrect_code, wrong_headers, chapter))
+  expect_true(.jrnotes$error)
+  .jrnotes$error = FALSE
 })
