db4698d69ff126a67bb801716ddebbc4f2d20d77
Colin Gillespie <csgillespie@gmail.com>
2020-09-28 13:46:33 +0100

    Use symbol$warning

--- R/cli_theme.R
+++ R/cli_theme.R
@@ -16,7 +16,7 @@ get_cli_theme = function() {
     .alert = list(before = paste0(symbol$arrow_right, " ")),
     `.alert-success` = list(before = paste0(crayon::green(symbol$tick), " "), color = "green"),
     `.alert-danger` = list(before = paste0(crayon::red(symbol$cross), " "), color = "red"),
-    `.alert-warning` = list(before = paste0(crayon::yellow("!"), " ")),
+    `.alert-warning` = list(before = paste0(crayon::yellow(symbol$warning), " ")),
     `.alert-info` = list(before = paste0(crayon::cyan(symbol$info), " "))
   )
   theme
