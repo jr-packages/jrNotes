0942dc07b74697ae41daa4b53a928b741489a5ff
Jack Walton <jack@jumpingrivers.com>
2020-09-30 15:10:37 +0100

    use CSGs url_exists

--- R/check_urls.R
+++ R/check_urls.R
@@ -1,45 +1,48 @@
 globalVariables(c("X1", "X2", "X3"))
 #' @importFrom httr GET
 #' @importFrom crayon yellow red green blue
-#' @importFrom RCurl url.exists
 check_urls = function() {
   if (!required_texlive(2017)) return(invisible(NULL))
 
   # Hack to detect internet connection on laptops
-  if (httr::GET("www.google.com")$status != 200) {
+  if (httr::http_error("www.google.com")) {
     msg_info("No internet connection - skipping URL check")
     return(invisible(NULL))
+  } else {
+    msg_start("Checking URLS...check_urls()")
   }
-  msg_start("Checking URLS...check_urls()")
+
   tokens = read_tokens()
+  # Get urls from extractor.csv
   urls = dplyr::filter(tokens, X1 == "url")$X3 #nolint
 
-  # Old fashioned URL grep
-  # No URLs gives a warning
+  df_status = t(data.frame(sapply(urls, url_exists, USE.NAMES = TRUE)))
 
-  # Replace {-} with - for BASH formatted lines starting with git
-  system2("sed",
-          args = c("-i", shQuote("/^\\FunctionTok{git}.*/d ; s/{-}/-/g"),
-                   "extractor-tmp.tex"))
-
-  grepped_url = suppressWarnings(system2("grep",
-                                         c('-Eo "(http|https)://[a-zA-Z0-9./?=_-]*"', "extractor-tmp.tex"), #nolint
-                                         stdout = TRUE))
-  urls = unique(c(urls,  grepped_url))
-  url_statuses = RCurl::url.exists(urls)
-  for (url in names(url_statuses)) {
-    if (url_statuses[url]) {
+  for (url in rownames(df_status)) {
+    if (df_status[url,]$exists) {
       msg_success(url, padding = TRUE)
     } else {
-      msg_error(url, padding = TRUE)
+      msg_error(glue::glue(url, df_status[url,]$message), padding = TRUE)
     }
   }
 
   # If any URL
-  if (all(url_statuses)) {
+  if (all(as.integer(df_status[,"exists"]))) {
     msg_success("URLs look good")
   } else {
     msg_error("Fix broken URLS")
   }
   return(invisible(NULL))
+}
+
+url_exists = function(url) {
+  ping = try(httr::HEAD(url), silent = TRUE)
+  if (class(ping) == "try-error") {
+    exists = FALSE
+    message = ping[1]
+  } else {
+    exists = ping$status %in% 200:205
+    message = httr::http_status(ping$status)$message
+  }
+  list("exists" = exists, "message" = message)
 }
