dea4afe6bd01708395bd3cc04c89f3f746ccfc9f
Jack Walton <jack@jumpingrivers.com>
2020-09-30 16:01:52 +0100

    Make status code human readable; allow bash chunk urls

--- R/check_urls.R
+++ R/check_urls.R
@@ -6,7 +6,7 @@ url_info = function(url) {
     status = ping[1]
     url_exists = FALSE
   } else {
-    status = ping$status
+    status = httr::http_status(ping$status)$message
     url_exists = ping$status %in% 200:210
   }
   list(exists = url_exists, url = url, status = status)
@@ -27,20 +27,26 @@ check_urls = function() {
   urls = dplyr::filter(tokens, X1 == "url")$X3 #nolint
 
   msg_info("Checking explict urls...", padding = TRUE)
-  url_statuses = TRUE
+  urls_good = TRUE
   for (url in unique(urls)) {
     resp = url_info(url)
     if (resp$exists || is_gitlab()) {
       msg_success(glue::glue("{resp$status}: {resp$url}"), padding = TRUE)
     } else {
       msg_error(glue::glue("{resp$status}: {resp$url}"), padding = TRUE)
-      url_statuses = FALSE
+      urls_good = FALSE
     }
   }
 
+  # Grab url's (allow {-} for URLs from bash chunks)
   grep_urls = suppressWarnings(system2("grep",
-                                         c('-Eo "(http|https)://[a-zA-Z0-9./?=_-]*"', "extractor-tmp.tex"), #nolint
+                                         c("-Eo", "'(http|https)://([a-zA-Z0-9./?=_-]|{-})*'",
+                                           "main.tex"), #nolint
                                          stdout = TRUE))
+
+  # Strip {} from bash chunk URLs ({} invalid URL characters)
+  grep_urls = gsub("\\{|\\}", "", grep_urls)
+
   urls = grep_urls[!(grep_urls %in% urls)]
   msg_info("Checking other urls...", padding = TRUE)
   for (url in unique(urls)) {
@@ -53,7 +59,7 @@ check_urls = function() {
   }
 
   # If any URL
-  if (all(url_statuses) || is_gitlab()) {
+  if (urls_good || is_gitlab()) {
     msg_success("URLs look good")
   } else {
     msg_error("Fix broken URLS")
